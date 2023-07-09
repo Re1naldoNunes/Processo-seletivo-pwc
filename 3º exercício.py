9c0afb3cbbcb7f37da98a9eeea22ebf0b0bf114d
Re1naldoNunes <r31n4ld0nun3s@gmail.com>
2023-07-09 18:18:22 -0300

    Corrigindo saída para ser string

--- 3º exercício.py
+++ 3º exercício.py
@@ -1,14 +1,16 @@
-frase = input ("Digite uma frase:" )
-frase = frase.replace (" ", "").lower()
+
+frase = input("Digite uma frase: ")
+frase = frase.replace(" ", "").lower()
 palindromos = []
 for i in range(len(frase)):
-    for j in range (i + 1, len(frase) + 1):
+    for j in range(i + 1, len(frase) + 1):
         substring = frase[i:j]
-        if len(substring) >= 3 and substring == substring [::-1]:
+        if len(substring) >= 3 and substring == substring[::-1]:
             palindromos.append(substring)
 
 if len(palindromos) > 0:
-    for palindromo in palindromos:
-        print (palindromo)
+    saida = "Palíndromos encontrados:\n" + "\n".join(palindromos)
 else:
-    print ("Nenhuma substring palindrôma foi encontrada")
+    saida = "Nenhuma substring palindrôma foi encontrada"
+
+print(saida)
